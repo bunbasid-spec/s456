19f86239816fcf8c09977e3d56687639f03caee5
bunbasid-spec <bunbasidyusuf@gmail.com>
2026-07-23 15:25:04 +0200

    Update LiveActivity.kt

--- app/src/main/java/com/example/iptvplayer/LiveActivity.kt
+++ app/src/main/java/com/example/iptvplayer/LiveActivity.kt
@@ -13,21 +13,15 @@ class LiveActivity : AppCompatActivity() {
         val rvCategories = findViewById<RecyclerView>(R.id.rvCategories)
         val rvChannels = findViewById<RecyclerView>(R.id.rvChannels)
 
-        // 1. Ortadan Odaklı Kaydırma Mantıklarını Atıyoruz
+        // Ortadan Odaklı Kaydırma Mantığı
         rvCategories.layoutManager = FocusCenteredLinearLayoutManager(this)
         rvChannels.layoutManager = FocusCenteredLinearLayoutManager(this)
 
-        // 2. Test İçin Örnek Veriler
+        // Test Verileri
         val dummyCategories = listOf("Ulusal", "Spor", "Sinema", "Belgesel", "Haber", "Çocuk", "Müzik", "Eğlence", "Dizi", "Uluslararası", "Yerel")
         val dummyChannels = listOf("TRT 1 HD", "ATV HD", "Kanal D HD", "Show TV HD", "Star TV HD", "TV8 HD", "NOW TV HD", "Kanal 7 HD", "A Haber HD", "TRT Spor HD", "BeIN Sports 1", "S Sport HD", "National Geographic")
 
-        // 3. Adapter'ları Bağlıyoruz
-        rvCategories.adapter = LiveRowAdapter(dummyCategories) { selectedCategory ->
-            // Kategoriye tıklandığında yapılacak işlem
-        }
-
-        rvChannels.adapter = LiveRowAdapter(dummyChannels) { selectedChannel ->
-            // Kanala tıklandığında (Tam ekran yapma vb.)
-        }
+        rvCategories.adapter = LiveRowAdapter(dummyCategories) { }
+        rvChannels.adapter = LiveRowAdapter(dummyChannels) { }
     }
 }
